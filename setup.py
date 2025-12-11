571985b3be6b3989284e3622f3ebc862bc408b57
saul <saul@gfz-potsdam.de>
2025-12-11 10:01:26 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scocto",
-    version="0.0.5"
+    name="scoctoloc",
+
+    description="Real-time event association/location for SeisComP using PyOcto",
+
+    version="0.0.6",
+
+    url='https://github.com/jsaul/scoctoloc',
+
+    author='Joachim Saul',
+
+    author_email='saul@gfz.de',
+
+    license='AGPLv3',
+
+    keywords='SeisComP, PyOcto, real-time seismology, earthquake location, phase association',
+
+    provides=["scocto"],
+
+    install_requires=['pyocto', 'seiscomp', 'numpy'],
+
+    python_requires='>=3',
+
 )
